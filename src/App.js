350088738cfda163160625e5ea5a9336d73b25a2
Guilherme Irie <guilherme.claudio@dynamox.net>
2023-08-24 20:37:00 -0300

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          OEEEEEEEEEEEEEOoeoeoeoe
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <img src={'https://media.tenor.com/ABeVmJ3y2WQAAAAM/cat-dancing-meme-dancing.gif'} alt={'cat dancing'}/>
       </header>
     </div>
   );
